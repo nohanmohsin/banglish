2efea5a5ddd2097af19bd50bd9f27774f8494a70
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2024-03-28 20:45:01 +0600

    idonno

--- app/learn/[slug]/page.jsx
+++ app/learn/[slug]/page.jsx
@@ -1,11 +1,15 @@
 import ArticleNav from "@/components/ArticleNav";
+import dynamic from "next/dynamic";
 
 export async function generateMetadata({ params }) {
   const { metaData } = await import(`/content/docs/${params.slug}.mdx`);
   return { title: metaData.title };
 }
 const Page = async ({ params }) => {
-  const Content = (await import(`/content/docs/${params.slug}.mdx`)).default;
+  const Content = dynamic(
+    import(`/content/docs/${params.slug}.mdx`).then((module) => module.default)
+  );
+
   console.log(Content);
   return (
     <div className="w-full max-w-auto prose prose-img:rounded py-8 pr-8 new-sm:px-8">
