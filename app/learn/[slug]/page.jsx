8f01208d2422e680b0012b82ee12e9db1fdcc653
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2024-03-28 19:47:18 +0600

    amogus

--- app/learn/[slug]/page.jsx
+++ app/learn/[slug]/page.jsx
@@ -9,7 +9,7 @@ const Page = async ({ params }) => {
   return (
     <div className="w-full max-w-auto prose prose-img:rounded py-8 pr-8 new-sm:px-8">
       <Content />
-      <ArticleNav currentSlug={params.slug} />
+      {/* <ArticleNav currentSlug={params.slug} /> */}
     </div>
   );
 };
