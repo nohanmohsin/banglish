3d46f8ecf9cd3d88319ada2e32d023e6f103f213
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2024-03-28 20:29:23 +0600

    found it

--- app/learn/[slug]/page.jsx
+++ app/learn/[slug]/page.jsx
@@ -5,7 +5,7 @@ export async function generateMetadata({ params }) {
   return { title: metaData.title };
 }
 const Page = async ({ params }) => {
-  const Content = await import(`/content/docs/${params.slug}.mdx`);
+  const Content = (await import(`/content/docs/${params.slug}.mdx`)).defaultX;
   console.log(Content);
   return (
     <div className="w-full max-w-auto prose prose-img:rounded py-8 pr-8 new-sm:px-8">
