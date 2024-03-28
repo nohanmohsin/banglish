fdaf2d97623905601e4c961df440d062c72c074b
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2024-03-28 19:57:04 +0600

    amogus 2

--- app/learn/[slug]/layout.js
+++ app/learn/[slug]/layout.js
@@ -5,7 +5,7 @@ const layout = ({ children, params }) => {
   return (
     <main className={"flex gap-4 new-sm:flex-col"}>
       <NavBar link={"learn"} />
-      <SideBar params={params} />
+      {/* <SideBar params={params} /> */}
       {children}
     </main>
   );
