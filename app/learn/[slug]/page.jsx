c8b8369f2df635234a163ddb6eacbd679bb13e9b
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2024-03-28 20:02:43 +0600

    all deleted

--- app/learn/[slug]/page.jsx
+++ app/learn/[slug]/page.jsx
@@ -8,7 +8,7 @@ const Page = async ({ params }) => {
   const Content = (await import(`/content/docs/${params.slug}.mdx`)).default;
   return (
     <div className="w-full max-w-auto prose prose-img:rounded py-8 pr-8 new-sm:px-8">
-      <Content />
+      {/* <Content /> */}
       {/* <ArticleNav currentSlug={params.slug} /> */}
     </div>
   );
